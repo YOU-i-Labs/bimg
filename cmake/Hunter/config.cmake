1bd156908cb6d9d3a00765aa7de8dfc563c046de
Brandon Turner <brandon.turner@youi.tv>
2019-08-26 12:39:39 -0400

    pointing to latest bx release

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,4 +1,4 @@
 hunter_config(bx
-    URL https://github.com/YOU-i-Labs/bx/archive/v1.0.0-youi-4.tar.gz
-    SHA1 239af90adf75dfccc7bd65e393d57ebcc2e723a8
+    URL https://github.com/YOU-i-Labs/bx/archive/v1.0.0-youi-5.tar.gz
+    SHA1 f1287af6ab7125f7626aed943996f1364489d51f
 )
